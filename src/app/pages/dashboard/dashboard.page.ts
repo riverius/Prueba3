6f621e6ea6af54ccc798a39f7db16a933f5009a4
diegos <die.marinf@duocuc.cl>
2023-11-26 20:19:07 -0300

    Comit diego

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { StateService } from '../../services/state.service';
 import { Router } from '@angular/router';
+import { PhotoService } from 'src/app/services/photo.service';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -10,12 +12,16 @@ import { Router } from '@angular/router';
 export class DashboardPage implements OnInit {
   title: string = 'Panel de Alumno';
   username:string = '';
+  photoService: any;
 
-  constructor(private stateService:StateService, private router:Router) { }
+  constructor(private stateService:StateService, private router:Router, public photoservice:PhotoService ) { }
 
   ngOnInit() {
     this.stateService.username.subscribe((value) => {
       this.username = value;
     });
+  }
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
   }
 }
