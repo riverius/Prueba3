6ff013509151c3ca67564ce4abff4488e13e145d
Orel Naranjo <or.naranjo@duocuc.cl>
2023-11-26 23:11:43 -0300

    Agregado Scanner QR

--- src/app/pages/qrscanner/qrscanner.page.ts
+++ src/app/pages/qrscanner/qrscanner.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BarcodeScanner, Barcode, IsGoogleBarcodeScannerModuleAvailableResult } from '@capacitor-mlkit/barcode-scanning';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-qrscanner',
@@ -6,10 +8,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./qrscanner.page.scss'],
 })
 export class QrscannerPage implements OnInit {
+  isSupported = false;
+  barcodes: Barcode[] = [];
 
-  constructor() { }
+  constructor(private alertController: AlertController) {}
 
   ngOnInit() {
+    BarcodeScanner.isSupported().then((result) => {
+      this.isSupported = result.supported;
+    });
+  }
+
+  async scan(): Promise<void> {
+    const granted = await this.requestPermissions();
+    if (!granted) {
+      this.presentAlert();
+      return;
+    }
+    const isAvailable: IsGoogleBarcodeScannerModuleAvailableResult = await BarcodeScanner.isGoogleBarcodeScannerModuleAvailable();
+    if (!isAvailable.available) {
+      await BarcodeScanner.installGoogleBarcodeScannerModule();
+    }
+    const { barcodes } = await BarcodeScanner.scan();
+    this.barcodes.push(...barcodes);
+  }
+
+  async requestPermissions(): Promise<boolean> {
+    const { camera } = await BarcodeScanner.requestPermissions();
+    return camera === 'granted' || camera === 'limited';
+  }
+
+  async presentAlert(): Promise<void> {
+    const alert = await this.alertController.create({
+      header: 'Permiso denegado',
+      message: 'Conceda permiso de cámara para usar el escáner de código de barras.',
+      buttons: ['OK'],
+    });
+    await alert.present();
   }
 
 }
