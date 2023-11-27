ab849187ea4bc4c53df9f8f923771c8a3e57af69
riverius <ma.escobarl@duocuc.cl>
2023-11-26 23:41:57 -0300

    jsons

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'EP1',
+  appName: 'Prueba3',
   webDir: 'www',
   server: {
     androidScheme: 'https'
