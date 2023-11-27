bf59644f6bd1055082ca5d256523760c04c722b3
riverius <ma.escobarl@duocuc.cl>
2023-11-26 21:54:16 -0300

    bye photo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+
+import { QRCodeModule } from 'angularx-qrcode';
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -1,27 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { StateService } from '../../services/state.service';
 import { Router } from '@angular/router';
-import { PhotoService } from 'src/app/services/photo.service';
-
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.scss'],
+  
 })
 export class DashboardPage implements OnInit {
   title: string = 'Panel de Alumno';
   username:string = '';
   photoService: any;
 
-  constructor(private stateService:StateService, private router:Router, public photoservice:PhotoService ) { }
+  qrText: string = '';
+  scannerDevice: string | undefined;
+  scannedValue: string | undefined;
+
+  constructor(private stateService:StateService, private router:Router ) { }
 
   ngOnInit() {
     this.stateService.username.subscribe((value) => {
       this.username = value;
     });
-  }
-  addPhotoToGallery() {
-    this.photoService.addNewToGallery();
   }
 }
