45df639ce98e2dc5f608e3c000216da92745a0d9
riverius <ma.escobarl@duocuc.cl>
2023-11-27 22:08:50 -0300

    loginspec

--- src/app/pages/login/login.page.spec.ts
+++ src/app/pages/login/login.page.spec.ts
@@ -1,39 +1,83 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
 import { LoginPage } from './login.page';
 import { StateService } from '../../services/state.service';
 import { Router } from '@angular/router';
-import { of } from 'rxjs';
-import { RouterTestingModule } from '@angular/router/testing';
 
 describe('LoginPage', () => {
   let component: LoginPage;
   let fixture: ComponentFixture<LoginPage>;
-  let stateService: StateService;
-  let router: Router;
+  let stateService: jasmine.SpyObj<StateService>;
+  let router: jasmine.SpyObj<Router>;
 
-  beforeEach(async() => {
+  beforeEach(async(() => {
+    const stateServiceSpy = jasmine.createSpyObj('StateService', ['login']);
+    const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+
     TestBed.configureTestingModule({
+      declarations: [LoginPage],
       imports: [RouterTestingModule],
-      declarations: [ LoginPage ],
       providers: [
-        {
-          provide: StateService,
-          useValue: {
-            login: () => Promise.resolve({ tipo: 'profesor' })
-          }
-        }
-      ]
+        { provide: StateService, useValue: stateServiceSpy },
+        { provide: Router, useValue: routerSpy },
+      ],
     }).compileComponents();
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(LoginPage);
     component = fixture.componentInstance;
-    stateService = TestBed.inject(StateService);
-    router = TestBed.inject(Router);
-    fixture.detectChanges();
+    stateService = TestBed.inject(StateService) as jasmine.SpyObj<StateService>;
+    router = TestBed.inject(Router) as jasmine.SpyObj<Router>;
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should call login method on button click', () => {
+    spyOn(component, 'login');
+    const button = fixture.debugElement.query(By.css('button'));
+    button.triggerEventHandler('click', null);
+    expect(component.login).toHaveBeenCalled();
+  });
+
+  it('should navigate to dashboard for professor', async(() => {
+    const user = { tipo: 'profesor' };
+    stateService.login.and.returnValue(Promise.resolve(user));
+
+    component.username = 'test';
+    component.password = 'password';
+    fixture.detectChanges();
+
+    fixture.whenStable().then(() => {
+      const button = fixture.debugElement.query(By.css('button'));
+      button.triggerEventHandler('click', null);
+
+      fixture.whenStable().then(() => {
+        expect(router.navigate).toHaveBeenCalledWith(['dashboard']);
+      });
+    });
+  }));
+
+  it('should navigate to qrscanner for student', async(() => {
+    const user = { tipo: 'alumno' };
+    stateService.login.and.returnValue(Promise.resolve(user));
+
+    component.username = 'test';
+    component.password = 'password';
+    fixture.detectChanges();
+
+    fixture.whenStable().then(() => {
+      const button = fixture.debugElement.query(By.css('button'));
+      button.triggerEventHandler('click', null);
+
+      fixture.whenStable().then(() => {
+        expect(router.navigate).toHaveBeenCalledWith(['qrscanner']);
+      });
+    });
+  }));
 });
